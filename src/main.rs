efbc7b2ea867c3df54cc308685f733ad2f9fc945
Edward Chung <munjo5746@gmail.com>
2021-03-05 22:44:46 -0500

    finding branch that has * in it

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,15 @@ use std::process::Command;
 fn main() {
     // get current branch name by running "grep '^\*' | cut -d' ' -f2 | tr -d '\n'"
     let git_branch = Command::new("git").arg("branch").output().expect("fail");
-    let branches = String::from_utf8(git_branch.stdout).unwrap();
-    println!("{}", branches)
+    let output_branches = String::from_utf8(git_branch.stdout).unwrap();
+    let output = output_branches
+        .split("\n")
+        .collect::<Vec<&str>>()
+        .into_iter()
+        .find(|line| line.contains("*"))
+        .unwrap();
+
+    println!("{}", output)
 
     // match String::from_utf8(output.stdout) {
     //     Ok(content) => { println!("output: {}", content)}
