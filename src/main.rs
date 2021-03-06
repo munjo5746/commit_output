6697cf50c04612d1c137a3fe3abb0d9dea3115c6
Edward Chung <munjo5746@gmail.com>
2021-03-05 23:00:01 -0500

    printing commit info

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,25 @@ fn main() {
     // get current branch name by running "grep '^\*' | cut -d' ' -f2 | tr -d '\n'"
     let git_branch = Command::new("git").arg("branch").output().expect("fail");
     let output_branches = String::from_utf8(git_branch.stdout).unwrap();
-    let output = output_branches
+    let current_branch = output_branches
         .split("\n")
         .collect::<Vec<&str>>()
         .into_iter()
         .find(|line| line.contains("*"))
         .unwrap();
-
-    println!("{}", output)
-
+    print!("\n\n");
+    println!(
+        "Your current branch: {}",
+        current_branch.replace("*", "").trim()
+    );
+    println!(
+        "Your ticket number: {}",
+        current_branch.replace("*", "").trim()
+    );
+    println!(
+        "Your ticket number: https://theconstellationagency.atlassian.net/browse/{}",
+        current_branch.replace("*", "").trim()
+    );
     // match String::from_utf8(output.stdout) {
     //     Ok(content) => { println!("output: {}", content)}
     //     Err(err) => { panic!(err)}
