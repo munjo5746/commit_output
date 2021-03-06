9bb69b1f53a2558af758a66b887c11a2e03a8ea7
Edward Chung <munjo5746@gmail.com>
2021-03-05 22:37:56 -0500

    outputing branch from Command output

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use std::process::Command;
 
 fn main() {
     // get current branch name by running "grep '^\*' | cut -d' ' -f2 | tr -d '\n'"
-    let output = Command::new("echo").arg("testing").output().expect("failed to get current branch name");
-    match String::from_utf8(output.stdout) {
-        Ok(content) => { println!("output: {}", content)}
-        Err(err) => { panic!(err)}
-    }
+    let git_branch = Command::new("git").arg("branch").output().expect("fail");
+    let branches = String::from_utf8(git_branch.stdout).unwrap();
+    println!("{}", branches)
+
+    // match String::from_utf8(output.stdout) {
+    //     Ok(content) => { println!("output: {}", content)}
+    //     Err(err) => { panic!(err)}
+    // }
 }
